21b1dc3c5e992c19225f9a4c0233e4ec8703cbc9
DavieK2 <daviekingz.dk@gmail.com>
2023-10-01 15:33:10 +0100

    Update

--- app/Console/Commands/SyncOnlineDatabaseToLocalCommand.php
+++ app/Console/Commands/SyncOnlineDatabaseToLocalCommand.php
@@ -16,76 +16,81 @@ class SyncOnlineDatabaseToLocalCommand extends Command
 
     public function handle()
     {
-        set_time_limit(0);
+        try {
+
+            set_time_limit(0);
         
-        $request = Http::get(env('APP_URL').'/api/sync-to-local');
-
-        $responses = $request->json();
-        
-        // $this->info( json_encode( $responses ));
-
-        if( ! $request->ok() ){
-
-            return ;
-        }
-
-       foreach( $responses as $table => $response ){
-
-            $path = "sync_dl/$table";
-
-            if( ! file_exists( public_path($path) ) ){
-                        
-                mkdir( public_path($path), recursive: true );
+            $request = Http::get(env('APP_URL').'/api/sync-to-local');
+    
+            $responses = $request->json();
+    
+            if( ! $request->ok() ){
+    
+                return ;
             }
-
-            $outputPath = public_path("$path/$table".'_'.now()->format('Y_m_d_H_i_s').".csv");
-
-            foreach ($response as $data) {
-            
-                $output = Process::run("curl -o $outputPath ".$data['sync_path'] )->errorOutput();
-
-                $this->info($output);
-
-                if( $output ){
-
-                    SimpleExcelReader::create($outputPath)->getRows()->each(function($row) use($table){
-
-                        $row = collect($row)->map(function($value) {
-
-                            $value = @unserialize($value) ? unserialize($value) : $value;
-                            $value = is_array($value) ? json_encode($value) : $value;
-                            $value = $value == "" ? null : $value;
-
-                            return $value;
-
-                        })->toArray();
-
-                        $row['is_synced'] = true;
-
-                        if( isset( $row['uuid'] ) ){
-
-                            $updateColumn = ['uuid' => $row['uuid'] ];
-
-                        }else{
-
-                            $updateColumn = $row;
-                        }
-                        
-                        Schema::disableForeignKeyConstraints();
-                       
-                        DB::table($table)->updateOrInsert( $updateColumn , $row);  
-                        
-                    });
-
-                    $request = Http::post('http://172.17.0.1/api/sync-to-local-confirm', ['id' => $data['id'] ] );
-                    
-                    $this->info( json_encode( $request->json() ) );
+    
+           foreach( $responses as $table => $response ){
+    
+                $path = "sync_dl/$table";
+    
+                if( ! file_exists( public_path($path) ) ){
+                            
+                    mkdir( public_path($path), recursive: true );
                 }
+    
+                $outputPath = public_path("$path/$table".'_'.now()->format('Y_m_d_H_i_s').".csv");
+    
+                foreach ($response as $data) {
+                
+                    $output = Process::run("curl -o $outputPath ".$data['sync_path'] )->errorOutput();
+    
+                    $this->info($output);
+    
+                    if( $output ){
+    
+                        SimpleExcelReader::create($outputPath)->getRows()->each(function($row) use($table){
+    
+                            $row = collect($row)->map(function($value) {
+    
+                                $value = @unserialize($value) ? unserialize($value) : $value;
+                                $value = is_array($value) ? json_encode($value) : $value;
+                                $value = $value == "" ? null : $value;
+    
+                                return $value;
+    
+                            })->toArray();
+    
+                            $row['is_synced'] = true;
+    
+                            if( isset( $row['uuid'] ) ){
+    
+                                $updateColumn = ['uuid' => $row['uuid'] ];
+    
+                            }else{
+    
+                                $updateColumn = $row;
+                            }
+                            
+                            Schema::disableForeignKeyConstraints();
+                           
+                            DB::table($table)->updateOrInsert( $updateColumn , $row);  
+                            
+                        });
+    
+                        $request = Http::post('http://172.17.0.1/api/sync-to-local-confirm', ['id' => $data['id'] ] );
+                        
+                        $this->info( json_encode( $request->json() ) );
+                    }
+                }
+    
             }
+    
+            $this->info('completed');
 
+        } catch (\Throwable $th) {
+            
+            $this->info($th->getMessage());
         }
-
-        $this->info('completed');
     
     }
 }
